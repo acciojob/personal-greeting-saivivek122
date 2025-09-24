21945b99b26320c5f6b92fe322ba57e7560f19a8
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-09-24 10:16:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,23 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [value,setValue]=useState("")
+  function userInputValue(event){
+    let userInput=event.target.value
+    if(userInput!=""){
+      setValue("Hello,"+userInput)
+    }
+    else{
+      setValue("")
+    }
+    
   return (
     <div>
         {/* Do not remove the main div */}
+ 
+      <input onChange={userInputValue}/>
+      <p>{value}</p>
+    
     </div>
   )
 }
